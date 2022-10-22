ede0be0c4e0a6844ead4706204f51abcf8cadcbe
Waseem Senjer <waseem.senjer@gmail.com>
2022-10-22 15:15:41 +0200

    update changelog 1.7.8

--- wc-royal-mail.php
+++ wc-royal-mail.php
@@ -3,7 +3,7 @@
  * Plugin Name:       WooCommerce Royal Mail Shipping Calculator
  * Plugin URI:        https://wpruby.com
  * Description:       WooCommerce Royal Mail Shipping Calculator
- * Version:           1.7.7
+ * Version:           1.7.8
  * WC requires at least: 3.0
  * WC tested up to: 7.0
  * Author:            WPRuby
